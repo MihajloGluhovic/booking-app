5cfdbf47e1016f4f60b3f16b2c705c4ee3d1bece
Mihajlo Gluhovic <mihajlogluhovic@gmail.com>
2024-11-13 15:41:13 +0100

    Stop tracking environment.development.ts

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  // apiUrl: 'https://hotelapi.azurewebsites.net/api',
-  apiUrl: 'https://hotelapimanagement.azure-api.net/api',
-  apiKey: 'ba33c3e5e8024a8ba3698bc1b4a3e28b',
-  limit: 10,
-};
