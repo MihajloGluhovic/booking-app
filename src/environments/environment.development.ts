b1229a3eb2fad5fc103238dd111490d2a92dd170
Mihajlo Gluhovic <mihajlogluhovic@gmail.com>
2024-12-05 16:51:06 +0100

    Minor contact changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  // apiUrl: 'https://hotelapi.azurewebsites.net/api',
   apiUrl: 'https://hotelapimanagement.azure-api.net/api',
   apiKey: 'ba33c3e5e8024a8ba3698bc1b4a3e28b',
-  limit: 10,
 };
