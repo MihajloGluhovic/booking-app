d9a4a519ac4c3dddbde86b3267132a94eed9dfda
Mihajlo Gluhovic <mihajlogluhovic@gmail.com>
2025-02-10 15:45:10 +0100

    Api Changed and small tweaks

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://hotelapimanagement.azure-api.net/api',
+  apiUrl: 'https://hotelappapi-guu7.onrender.com/api',
   apiKey: 'ba33c3e5e8024a8ba3698bc1b4a3e28b',
 };
